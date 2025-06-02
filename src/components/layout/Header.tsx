0dedbb0a57a14ddb0d0a05d73b76cd1f6cdbf8ac
rui0421 <rui.04215@icloud.com>
2025-06-03 00:30:47 +0900

    change

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="bg-white shadow-sm">
-              <div className="max-w-7xl mx-auto py-3">
-          <div className=" flex flex-col items-center text-center cursor-pointer">
-            <p className="text-xs text-gray-500 tracking-widest font-great">Hair Salon</p>
-            <h1 className="text-lg font-luana tracking-widest text-gray-700">
-              LUANA • S • MIRUTO
-            </h1>
-          </div>
-          </div>
+      <div className="max-w-7xl mx-auto py-3">
+        <div className="flex flex-col items-center text-center cursor-pointer">
+          <p className="text-xs text-gray-500 tracking-widest font-great">Hair Salon</p>
+          <h1
+            className="text-lg font-luana tracking-widest text-gray-700"
+            onClick={() => navigate('/')}
+            style={{ userSelect: 'none' }}
+          >
+            LUANA • S • MIRUTO
+          </h1>
+        </div>
+      </div>
     </header>
   );
 };
